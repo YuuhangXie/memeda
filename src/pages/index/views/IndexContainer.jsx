ea2d38be32abb2f092d82709692b0ab597285b4e
xyhthink <yuuhangxie@gmail.com>
2019-10-08 11:22:20 +0800

    10-08-merge

--- src/pages/index/views/IndexContainer.jsx
+++ src/pages/index/views/IndexContainer.jsx
@@ -30,7 +30,7 @@ import profileActive from 'images/tablebar/profile-active.png'
 
 // 实际引用组件位置
 // import { Chat } from '../chat/'
-// import Home from '../home/'
+// import { Home } from '../home/'
 import  { Community } from '../community/'
 // import Fund from '../fund/'
 import { Profile } from '../profile/'
