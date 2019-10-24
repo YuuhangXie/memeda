fd3e9bc7b3da51105b71b85a7d6562bb54913029
xyhthink <yuuhangxie@gmail.com>
2019-10-24 22:35:42 +0800

    1

--- src/pages/index/home/view/HomeUI.jsx
+++ src/pages/index/home/view/HomeUI.jsx
@@ -7,19 +7,19 @@ export default (props) => {
             <div className="home-title">
                 <div className="femaleAva avatar">
                     <div className="avatar-box">
-                        <img src={props.userInfo[1].head_img} alt="头像"/>
+                        <img style={{borderRadius: '50%'}} src={props.userInfo.head_img} alt="头像"/>
                     </div>
-                    <span>{props.userInfo[1].nickname}</span>
+                    <span>{props.userInfo.nickname}</span>
                 </div>
                 <div className="homeDate">
                     <span>我们已相爱</span>
-                    <span>{props.date}天</span>
+                    <span>1天</span>
                 </div>
                 <div className="maleAva avatar">
                     <div className="avatar-box">
-                        <img src={props.userInfo[0].head_img} alt="头像"/>
+                        <img style={{borderRadius: '50%'}} src={props.userInfo.bind_info.head_img} alt="头像"/>
                     </div>
-                    <span>{props.userInfo[0].nickname}</span>
+                    <span>{props.userInfo.bind_info.nickname}</span>
                 </div>
             </div>
         </div>
